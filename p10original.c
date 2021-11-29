9a88a4a8786e0d29c364f7b42af08e6c9c2f5e30
Mahesh S <95006793+maheshsp192@users.noreply.github.com>
2021-11-29 03:07:00 +0000

    addition of teo numers using function

--- p10original.c
+++ p10original.c
@@ -1,1 +1,16 @@
-
+#include <stdio.h>
+int main ()
+{
+  int num1 ,num2 ,add;
+  printf("Enter the two numbers: ");
+  scanf("%d %d , &num1 &num2");
+  add = sum(num1, num2);
+  printf("Addition of two number is : ");
+  return 0 ;
+}
+int sum (int num1 int num2)
+{
+  int num3;
+  num3 = num1 + num2;
+  return num3; p10roiginl.c
+}
